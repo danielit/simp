651bbb4215b94a17b793327d1330039f68d93f34
danielit <danielit@sina.com>
2014-03-15 01:44:37 +0800

    modify the Manage Tree

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,8 @@
 Ext.require(['Ext.grid.Panel', 'Ext.grid.*', 'Ext.window.Window', 'Ext.container.Viewport', 'Ext.layout.container.Border', 'Ext.state.*', 'Ext.data.*', 'Ext.tab.*', 'Ext.util.*', 'Ext.toolbar.Paging', 'Ext.String.*', 'Ext.selection.Model',
 ]);
-
+//this tab panel is for the center 
 var mgrTabpanel = Ext.create('Ext.tab.Panel', {
 	id: 'mgrTabpanel',
-	//name:"mgrTabpanel",
 	renderTo: Ext.getBody(),
 	enableTabScroll: true,
 	defaults: {
@@ -20,230 +19,6 @@ var mgrTabpanel = Ext.create('Ext.tab.Panel', {
 	}]
 });
 
-var testStuModelFields = ['stuid', 'name', 'birthday', 'gender', 'examid', 'headteac', 'identify', 'homephone'];
-
-function getAjaxProxy(Url, Reader, Writer) {
-	var testAjaxProxy = Ext.create('Ext.data.proxy.Ajax', {
-		url: Url,
-		reader: Reader,
-		// this is used to read the response data, use "GET"
-		writer: Writer // this is used to write date to server, use "POST" 
-	});
-
-	return testAjaxProxy;
-}
-
-var U = SERVER+"/getallstuinfo" ;
-//var U = "http://192.168.84.251:8888/getallstuinfo";
-var R = {
-	type: 'json',
-	root: 'users'
-}; //,'extraParams':{'testid':12345,'text':'helloworld'}} ;
-var testAjaxProxy = getAjaxProxy(U, R, {});
-// through set extra param ,we can send some information to server
-testAjaxProxy.setExtraParam("testid", 123456);
-testAjaxProxy.setExtraParam("text", "helloa");
-Ext.define('Student', {
-	extend: 'Ext.data.Model',
-	fields: testStuModelFields,
-	proxy: testAjaxProxy
-	/*
-    proxy:{
-        type:'ajax',
-        url:'http://127.0.0.1:8888/getallstuinfo',
-        reader:{
-            type:'json', // reader used to encode json data 
-            root:'users' // the way to find data in response way ,{"success":ture,"users":{...}}
-        }
-    }*/
-});
-
-var teststore = Ext.create('Ext.data.Store', {
-	model: 'Student',
-	autoLoad: false // load data from url which model setted
-});
-
-//var selectMode = new Ext.grid.RowSelectionModel({ singleSelect: true }) ;
-var selectMode = new Ext.selection.Model({
-	model: "SIMPLE"
-});
-var dataColumns = new Ext.grid.column.Column({
-	items: [{
-		xtype: 'rownumberer'
-	},
-	{
-		text: '学号',
-		dataIndex: 'stuid'
-	},
-	{
-		text: '姓名',
-		dataIndex: 'name'
-	},
-	{
-		text: '年龄',
-		dataIndex: 'birthady'
-	},
-	{
-		text: '性别',
-		dataIndex: 'gender'
-	},
-	{
-		text: '身份证',
-		dataIndex: 'identify'
-	},
-	{
-		text: '家庭联系方式',
-		dataIndex: 'homephone'
-	}],
-	defaults: {
-		align: 'center',
-		flex: 1
-	}
-});
-
-var testGrid = Ext.create('Ext.grid.Panel', {
-	xtype: 'grid',
-	title: 'Simpsons',
-	closable: true,
-	store: teststore,
-	loadMask: true,
-	autoHeight: true,
-	viewConfig: {
-		forceFit: true
-	},
-    defaults:{
-        align:'center',
-        flex:1,
-        //width: 50,
-        //maxWidth:200
-
-    },
-	border: true,
-	selModel: selectMode,
-    //columns: dataColumns,
-    columns:[{
-        header:'编号',
-        text:'编号',
-        width:60,
-		xtype: 'rownumberer'
-	},
-	{
-		header: '学号',
-		dataIndex: 'stuid'
-	},
-	{
-		header: '姓名',
-		dataIndex: 'name'
-	},
-	{
-		header: '年龄',
-		dataIndex: 'birthday'
-	},
-	{
-		header: '性别',
-		dataIndex: 'gender'
-	},
-	{
-		header: '身份证',
-		dataIndex: 'identify'
-	},
-	{
-		header: '家庭联系方式',
-		dataIndex: 'homephone'
-	}],
-    border: 5,
-	style: {
-		//borderColor: 'blue',
-		borderStyle: 'solid'
-	},
-	// paging bar on the bottom
-	bbar: Ext.create('Ext.PagingToolbar', {
-		store: teststore,
-		displayInfo: true,
-		displayMsg: 'Displaying topics {0} - {1} of {2}',
-		emptyMsg: "No topics to display",
-	}),
-	renderTo: Ext.getBody()
-});
-
-var testGrid2 = Ext.create('Ext.grid.Panel', {
-	id: 'testGrid2',
-	xtype: 'grid',
-	title: 'Simpsons',
-	closable: true,
-	store: Ext.data.StoreManager.lookup('simpsonsStore'),
-	columns: [{
-		header: '姓名',
-		dataIndex: 'name'
-	},
-	{
-		header: '年龄',
-		dataIndex: 'age'
-	},
-	{
-		header: '性别',
-		dataIndex: 'sex'
-	},
-	{
-		header: '身份证',
-		dataIndex: 'identify'
-	},
-	{
-		header: '联系方式',
-		dataIndex: 'phone'
-	},
-	{
-		header: 'Email',
-		dataIndex: 'email',
-		flex: 1
-	}],
-	renderTo: Ext.getBody()
-});
-//test getGridPanel
-var cols = [{
-	header: '姓名',
-	dataIndex: 'name'
-},
-{
-	header: '年龄',
-	dataIndex: 'age'
-},
-{
-	header: '性别',
-	dataIndex: 'sex'
-},
-{
-	header: '身份证',
-	dataIndex: 'identify'
-},
-{
-	header: '联系方式',
-	dataIndex: 'phone'
-},
-{
-	header: 'Email',
-	dataIndex: 'email',
-	flex: 1
-}];
-
-function getGridPanel(identify, ti, cols, closeb) {
-	// if the identify is already exist ,just return the obj
-	tmpGrid = Ext.getCmp(identify);
-	if (tmpGrid) {
-		return tmpGrid;
-	}
-	// else create a new one
-	var tmpGrid = Ext.create('Ext.grid.Panel', {
-		id: identify,
-		xtype: 'grid',
-		title: ti,
-		closable: !! closeb,
-		store: Ext.data.StoreManager.lookup('simpsonsStore'),
-		columns: cols,
-		renderTo: Ext.getBody()
-	});
-	return tmpGrid;
-}
 
 /*
  * add tab to a tabpanel
@@ -262,24 +37,9 @@ function add2tabpanel(tabs, obj) {
 	tabs.setActiveTab(obj);
     return ret ;
 }
-/*
-function getQuanWin(){
-    var quanWin = Ext.getCmp('quantest') ;
-    if (quanWin==null){
-        quanWin = Ext.create('Quan.window',{
-            title:'信息工程系量化管理平台',
-            id:'quantest',
-            itemId:'quantest',
-            xtype:'quancontainer',
-		    icon: 'static/pic/css/tabs.gif'
-        }) ; 
-    } 
-    return quanWin ;
-}
-*/
+
 function treeItemClick(view, record, item, index, e) {
-	//alert(record.raw.text) ;
-	var n = mgrList[record.raw.id];
+    
     var cmpId = record.raw.id ;
 
     if(cmpId=="q.weeksummary"){         
@@ -330,9 +90,7 @@ function treeItemClick(view, record, item, index, e) {
         attendWin.show() ; 
         
         setAttendWinsShow(false) ;
-
-    } else if (cmpId=="ss.loan"){
-    } else if (cmpId=="ss.loan"){
+} else if (cmpId=="ss.loan"){ } else if (cmpId=="ss.loan"){
         // student information
     } else if (cmpId=="si.baseinfo"){
         var siForm = getStuBaseInfoForm() ;
@@ -487,6 +245,7 @@ var mgrSysTree= Ext.create('Ext.tree.Panel', {
 	title: '系统管理',
 	deferRowRender: true,
 	useArrows: true,
+    //rootVisible:false,
 	listeners: {
 		itemclick: {
 			fn: treeItemClick
@@ -497,32 +256,55 @@ var mgrSysTree= Ext.create('Ext.tree.Panel', {
 		text: '系统管理',
 		expanded: true,
 		children: [{
-			id: 'sm.user',
+			id: 'sm.adduser',
 			text: '添加用户',
             leaf:true		
         },
+        {
+			id: 'sm.deluser',
+			text: '删除用户',
+            leaf:true		
+        },
 		{
-			id: 'sm.tips',
-			text: '添加公告',
+			id: 'sm.addnotice',
+			text: '添加通知/公告',
             leaf:true
         },
 	    {
-			id: 'sm.notice',
-			text: '添加通知',
+			id: 'sm.delnotice',
+			text: '删除通知/公告',
             leaf:true
-			
 		}]
 	}
 });
 
+var mgrQuitTree= Ext.create('Ext.tree.Panel', {
+	title: '退出系统',
+	deferRowRender: true,
+	useArrows: true,
+    rootVisible:false,
+	listeners: {
+		itemclick: {
+			fn: treeItemClick
+		},
+	},
+	root: {
+		id: 'sysQuitRoot',
+		//text: '系统管理',
+		expanded: true,
+		children: [{
+			id: 'sq.quit',
+			text: '退出',
+            leaf:true		
+        }]
+	}
+});
+
+
 
 Ext.onReady(function() {
     Ext.Loader.injectScriptElement("static/extjs/locale/ext-lang-zh_CN.js") ;
-	// setup the state provider, all state information will be saved to a cookie
-	//Ext.state.Manager.setProvider(Ext.create('Ext.state.CookieProvider'));
-	teststore.loadPage(1); // this would send params page=1&start=0&limit=25
-
-    
+	
 	Ext.create('Ext.container.Viewport', {
 		layout: {
 			type: 'border',
@@ -550,10 +332,12 @@ Ext.onReady(function() {
 			mgrTree,
             //mgrStuInfoTree,
             mgrSysTree,
-			{
+			/*{
 				title: '在线帮助',
 				html: 'Online Help'
-			}]
+			},*/
+            mgrQuitTree
+            ]
 		},
 		{
 			region: 'center',
@@ -563,4 +347,3 @@ Ext.onReady(function() {
 	});
     
 });
-

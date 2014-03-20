a5ab88a6dd3ba089cd8f99eedcfd7bb954af6746
danielit <danielit@sina.com>
2014-03-20 18:58:32 +0800

    this is used for news display, now it can not to translate the new line problem

---
+++ student/news_template.py
@@ -0,0 +1,15 @@
+NEWS_TEMPLATE='''
+<center>
+<table>
+    <tr align='center'>
+        <td><h1>%s</h1></td>
+    </tr>
+    <tr align='center'>
+        <td><h3>%s %s</h1></td>
+    </tr>
+    <tr align='left'>
+        <td><h2>%s<h2></td>
+    </tr>
+</table>
+</center>
+'''

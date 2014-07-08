e93230dc721976c2397fcd0b4e33b77e5171cd90
danielit <danielit@sina.com>
2014-07-08 21:21:30 +0800

    do recv and send msg callback func

--- static/js/chat.js
+++ static/js/chat.js
@@ -7,36 +7,36 @@ Ext.require(['Ext.data.*', 'Ext.tip.QuickTipManager', 'Ext.window.MessageBox']);
 var POS_X = 500 ;
 var POS_Y = 200 ;
 var lastChatWin = null ;
-//define the container for users 
-Ext.define('users.window', {
-	extend: 'Ext.container.Container',
-	alias: 'widget.userswin',
-	
-    initComponent: function() {
-		Ext.apply(this, {
-			padding: '0 0 0 0',
-            layout:'border',
-            closable:false, 
-			items: [
-                ]
-		});
-		this.callParent();
-	}
-});
-
+/**
+ * @desc : when one node of chatting list is clicked
+ *         pop up the chatting window
+ */
 function treeItemClick(view, record, item, index, e) {
-    nodeId = record.raw.id; //获取点击的节点id
-    nodeText = record.raw.text; //获取点击的节点text
-    chatWinId = 'chatwin2'+nodeId ;
-    chatWin = getChatForm(chatWinId,nodeText,'Good Good Study, Day Day Up ...','../pic/user123.jpg','../pic/'); 
+    nodeId = record.raw.id; //get node id, which is userid who is going to talk to
+    nodeText = record.raw.text; //get node text, which is name who is going to talk to
+    chatWinId = nodeId ; // the id of chating window is the target' id who is going to talk to
+    chatWin = getChatForm(chatWinId,nodeText,DEF_PERSONAL_SIGN,DEF_PERSONAL_PIC); 
     console.log(chatWin) ;
     if(lastChatWin!=null && lastChatWin != chatWin){
         lastChatWin.hide() ;
-        chatWin.show() ;
-        chatWin.setXY([POS_X,POS_Y]) ;
     }
+    chatWin.show() ;
+    chatWin.setXY([POS_X,POS_Y]) ;
     lastChatWin = chatWin ;
+    //recv msg of some body(whoes id is the same to the nodeId)
+    var ret = recvMsgs(nodeId) ;
+    if (ret){
+        // recv done
+    }else{
+        // recv failed
+    }
 }
+/**
+ * @desc : send msg to the chating server,
+ *          take advance of ajax of extjs
+ * @param : uid : the id of user who the msg will send to 
+ * @param : msg : The msg to send
+ */
 function sendMsg(uid,msg){
     var ret = true ;
     Ext.Ajax.request({                
@@ -50,20 +50,62 @@ function sendMsg(uid,msg){
         },
         method: 'POST',
         success: function (response, options) {
-		    //attend_info_store.remove(selection);
-            attend_info_store.load() ;
-            //setAttendWinShow('attendform',false) ;
-            ret = true ;
+            //doSendMsgCallback(response.response.Text); 
             Ext.MessageBox.alert('成功', '删除成功');
         },
         failure: function (response, options) {
             ret = false ;
+            // if send msg failed ,send it again
+            doSendMsgCallback(uid,msg,response.response.Text); 
             Ext.MessageBox.alert('失败', '请求超时或网络故障');
         }
     });
     return ret ;
 }
-function recvMsg(){
+/**
+ * @desc : do somthing when send msg to server failed
+ *          and then send msg again
+ */
+function doSendMsgCallback(uid,msg,jsonData){
+    var data = Ext.util.JSON.decode(jsonData) ;
+    sendMsg(uid,msg) ;
+}
+/**
+ * @desc : check if has msgs for me
+ *         if has call doHasMsgCallback
+ */
+function hasMsgs(){
+    var ret = true ;
+    Ext.Ajax.request({                
+        url: SERVER+'/Chat/check',
+        params: { 
+        },
+        method: 'get',
+        success:function (response, options) {
+            ret = true ;
+            Ext.MessageBox.alert('成功', 'check done');
+            doHasMsgsCallback(response.responseText) ;
+        },
+        failure: function (response, options) {
+            ret = false ;
+            Ext.MessageBox.alert('失败', 'check failed');
+        }
+    });
+    return ret ;
+
+} 
+/**
+ * @desc : when thereis msgs for me ,
+ *         check if the chatting window is opened ,recv then and display it
+ *               if the chatting window is not opend, set the node of chatting list sparking 
+ */
+function doHasMsgsCallback(jsonData){
+    var msgInfos = Ext.util.JSON.decode(jsonData) ;
+    console.log(msgInfos) ;
+    //
+    
+}
+function recvMsgs(uid){
     var ret = true ;
     Ext.Ajax.request({                
         url: SERVER+'/Chat/recv',
@@ -71,7 +113,7 @@ function recvMsg(){
             'userHeader': 'userMsg'
         },
         params: { 
-            //'to': 'idtest',
+            'from': uid,
             //'msg':'hello world'
         },
         method: 'get',
@@ -80,19 +122,19 @@ function recvMsg(){
             attend_info_store.load() ;
             //setAttendWinShow('attendform',false) ;
             ret = true ;
-            Ext.MessageBox.alert('成功', '删除成功');
+            //Ext.MessageBox.alert('成功', '成功');
             doRecvMsgCallback(response.responseText) ;
         },
         failure: function (response, options) {
             ret = false ;
-            Ext.MessageBox.alert('失败', '请求超时或网络故障');
+            //Ext.MessageBox.alert('失败', '请求超时或网络故障');
         }
     });
     return ret ;
 }
 function doRecvMsgCallback(jsonData){
     var jsonObj = Ext.util.JSON.decode(jsonData);
-    alert('in doRecvMsgCallback') ;
+    console.log(msgInfos) ;
 }
 
 function onSendClick(el,ev) {
@@ -101,14 +143,41 @@ function onSendClick(el,ev) {
     var talkWin = formPanel.getComponent('talk_win') ;
     var sendWin = formPanel.getComponent('send_win') ;
     var msg = sendWin.getValue() ;
-    msg = msg+"     <br/>" ;
-var msg_send_tpl = '<div style="text-align:right;align:right;margin:5px 5px 5px 5px"><tr><td>{0}<img src="../pic/rss.gif"/><br/><font size=4>{1}</font></td><tr/></div>';
+    //msg = msg+"     <br/>" ;
+    /*
+    var msg_send_tpl = '<div style="text-align:right;align:right;margin:5px 5px 5px 5px"><tr><td>{0}<img src="../pic/rss.gif"/><br/><font size=4>{1}</font></td><tr/></div>';
     var mmsg = Ext.String.format(msg_send_tpl,(new Date()).toLocaleString(),msg) ;
     talkWin.body.insertHtml('beforeEnd',mmsg);
-    talkWin.updateLayout() ;
+    //talkWin.updateLayout() ;
     talkWin.body.scroll('b', 10000, {duration: 0.1});
+    */
+    var fmsg = formatMsg(msg,"我",false) ;
+    setMsg2ChatWin(talkWin,fmsg) ;
     sendWin.setValue('') ;
     sendMsg(formPanel.id,msg) ;
+}
+function formatMsg(msg,who,isSend){
+    var msg_send_tpl = '<div style="text-align:right;align:right;margin:5px 5px 5px 5px"><tr><td>\<{1}\> {0}<img src="{2}"/><br/><font size=4 margin-right="5px">{3}</font></td><tr/></div>';
+    var msg_recv_tpl = '<div style="text-align:left; align:left; margin:5px 5px 5px 5px"><tr><td><img src="{2}"/>{0} \<{1}\>:<br/> <font size=4 margin-left="5px">{3}</font></td><tr/></div>';
+
+    var tpl = '' ;
+    if (isSend){
+        tpl = msg_send_tpl ;
+    }else{
+        tpl = msg_recv_tpl ;
+    }
+    var ret = Ext.String.format(tpl,(new Date()).toLocaleString(),who,PIC+DEF_PERSONAL_PIC,msg) ;
+    return ret ;
+}
+function setMsg2ChatWin(chatWin,msg){
+    //chatWin = Ext.getCmp(winId) ;
+    if (chatWin==null){
+        return false ;
+    }
+    chatWin.body.insertHtml('beforeEnd',msg) ;
+    //chatWin.updateLayout() ;
+    chatWin.body.scroll('b', 10000, {duration: 0.1});
+    return true ;
 }
 
 function onCloseClick(el,ev) {
@@ -200,16 +269,16 @@ Ext.define('Chat.Form', {
  * @param winId : id of the win
  * @param pSign : personal sign 
  * @param picture : picture of himself or herself
- * @param iPaht : icon path 
+ * @param icon : icon of the chating window 
  */
-function getChatForm(winId,winTitle,pSign,picture,iPath){
+function getChatForm(winId,winTitle,pSign,picture){
     var chatForm = Ext.getCmp(winId) ;
     if (chatForm == null){
         chatFrom = Ext.create('Chat.Form',{
             id:winId,
             personalSign:pSign,
-            picUrl:picture,
-            iconPath:iPath,
+            picUrl:PIC+picture,
+            iconPath:ICON,
             title:winTitle,
             renderTo:'chatzz',
             floatable:true,

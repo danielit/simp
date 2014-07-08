3d8bb2f69654a8ad8f236f16355e58934e24bbd2
danielit <danielit@sina.com>
2014-07-08 21:21:57 +0800

    add some config for chat

--- static/js/config.js
+++ static/js/config.js
@@ -1,4 +1,7 @@
-var HOST = "http://192.168.85.77" ; 
-//var HOST = "http://127.0.0.1" ;
+var HOST = "http://127.0.0.1" ;
 var PORT = "80" ;
 var SERVER = HOST + ":" + PORT  ;
+var ICON="../pic/" ;
+var PIC="../pic/" ;
+var DEF_PERSONAL_SIGN = "Good Good Study, Day Day Up!" ;
+var DEF_PERSONAL_PIC = "rss.gif" ;
